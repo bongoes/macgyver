33d713b4ece735252cb5bc436647c26d450b9ec8
Ciamé Andersson <my_bongo@hotmail.com>
2019-01-05 23:39:36 -0400

    Added labyrinth structure

--- macgyver.py
+++ macgyver.py
@@ -12,13 +12,33 @@ pygame.init()
 size = (300, 300)
 screen = pygame.display.set_mode(size)
 
-class Maze(pygame.sprite.Sprite):
+all_sprites_list = pygame.sprite.Group()
+
+class Tile(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         self.image = pygame.image.load("tile.png").convert()
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
- 
+
+    #def draw_layout(self, layout):
+        #for row in layout:
+            #for col in row:
+               #if layout[row][col] == 1:
+                   # self.rect.x = col * 20
+                   # self.rect.y = row * 20
+                   # all_sprites_list.add(self)
+
+class Maze():
+    def __init__(self,layout):
+        for i in range(len(layout)):
+            for j in range(len(layout[i])):
+                if layout[i][j] == 1:
+                    tile = Tile()
+                    tile.rect.x = j * 20
+                    tile.rect.y = i * 20
+                    all_sprites_list.add(tile)
+         
 pygame.display.set_caption("Help MacGyver escape!")
  
 # Loop until the user clicks the close button.
@@ -27,6 +47,24 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
+structure = [[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+                 [1,1,0,0,0,0,1,0,0,0,0,0,0,0,1],
+                 [0,0,0,0,1,1,1,0,0,0,0,0,0,0,1],
+                 [1,0,1,0,0,1,0,0,0,0,0,1,1,0,1],
+                 [1,0,0,0,0,0,0,0,1,0,0,1,1,0,1],
+                 [1,0,1,0,0,0,0,0,0,0,0,0,1,0,1],
+                 [1,1,1,1,1,1,0,0,0,0,0,0,1,0,1],
+                 [1,0,0,0,1,1,0,0,1,0,0,0,1,1,1],
+                 [1,0,0,0,0,0,0,0,1,0,0,0,1,0,1],
+                 [1,0,0,0,0,0,0,1,1,0,0,0,0,0,1],
+                 [1,0,1,1,1,1,1,1,1,0,1,0,0,0,1],
+                 [1,0,0,0,0,0,0,0,0,0,0,0,1,1,1],
+                 [1,0,0,1,1,1,0,0,0,0,0,0,1,0,0],
+                 [1,0,0,0,0,0,0,0,0,1,0,0,0,0,1],
+                 [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]]
+
+maze = Maze(structure)
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -34,12 +72,12 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
 
-    maze = Maze()
-    maze.rect.x = 10
-    maze.rect.y = 10
-    all_sprites_list = pygame.sprite.Group()
-    all_sprites_list.add(maze)
-    
+    #maze = Maze()
+    #maze.rect.x = 10
+    #maze.rect.y = 10
+    #all_sprites_list = pygame.sprite.Group()
+    #all_sprites_list.add(maze)
+
     # --- Game logic should go here
  
     # --- Screen-clearing code goes here
